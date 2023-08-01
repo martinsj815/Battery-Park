c559f808c0057d4036383730a5bf82faf00d6770
donghee1025 <doch@umich.edu>
2023-08-01 11:32:29 -0700

    Test dash for cell modeling code

--- main.py
+++ main.py
@@ -6,10 +6,68 @@ import dash
 import plotly.graph_objs as go
 import plotly.express as px
 
+app_name = os.getenv("APP_NAME", "Beyond-Cell")
 app = dash.Dash(
     external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True
 )
 
+app.title="Cell Design"
+server = app.server
+"""Homepage"""
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content"),]
+)
+
+index_page = html.Div(
+    [
+        html.Br(),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(html.H1(children=" Welcome to Cell Design "), width=5),
+                dbc.Col(width=5),
+            ],
+            justify="center",
+        ),
+        html.Br(),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H4(
+                                children=" Batteries are complex devices. To optimize cell performance and develop new types of battery chemistry for practical applications, it is critical to allocate our precious resources effectively. The modeling tools here can play a significant role in providing valuable information for developing plans and strategies"
+
+                            ),
+                            html.Div(
+                                [
+                                    dcc.Link(
+                                        html.Button(
+                                            "Home", id="home-button", className="p1"
+                                        ),
+                                        href=f"/{app_name}/",
+                                    ),
+                                    dcc.Link(
+                                        html.Button(
+                                            "Poutch", id="poutch-cell", className="p1"
+                                        ),
+                                        href=f"/{app_name}/Poutch",
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                    width=7,
+                ),
+                dbc.Col(width=3),
+            ],
+            justify="center",
+        ),
+        html.Br(),
+        html.Br(),
+    ]
+)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'check, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -17,5 +75,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run_server(debug=False)
 
